60b5d261500c7504cff622f646b3d33c00c9df74
Mridul Mahesh <mridulmahesh262@gmail.com>
2020-06-20 17:46:10 +0800

    Shifted all background logic into popup.js

--- background.js
+++ background.js
@@ -1,54 +1,54 @@
-const nameToLeanings = {
-  'bbc.com': 'left',
-  'cnn.com': 'left',
-  'foxnews.com': 'right',
-};
-const leaningsToName = { left: ['bbc.com', 'cnn.com'], right: ['foxnews.com'] };
+// const nameToLeanings = {
+//   'bbc.com': 'left',
+//   'cnn.com': 'left',
+//   'foxnews.com': 'right',
+// };
+// const leaningsToName = { left: ['bbc.com', 'cnn.com'], right: ['foxnews.com'] };
 
-const urlObject = (dataObject) => {
-  const urlArray = [];
-  for (i of Object.keys(dataObject)) {
-    urlArray.push({ hostSuffix: i });
-  }
+// const urlObject = (dataObject) => {
+//   const urlArray = [];
+//   for (i of Object.keys(dataObject)) {
+//     urlArray.push({ hostSuffix: i });
+//   }
 
-  return urlArray;
-};
+//   return urlArray;
+// };
 
-// const port = chrome.runtime.connect({ name: 'defaultPort' });
-const messageObject = { action: '', urlKey: '', urlLeaning: '' };
-chrome.webNavigation.onCompleted.addListener(
-  (details) => {
-    let urlKey;
-    for (i of Object.keys(nameToLeanings)) {
-      if (details.url.includes(i)) {
-        urlKey = i;
-        break;
-      }
-    }
-    console.log('Step 1');
-    // chrome.runtime.sendMessage(
-    //   {
-    //     action: 'urlDetails',
-    //     urlKey,
-    //     urlLeaning: nameToLeanings[urlKey],
-    //   },
-    //   (response) => {
-    //     // console.log(response.farewell);
-    //   }
-    // );
-    messageObject['action'] = 'urlDetails';
-    messageObject['urlKey'] = urlKey;
-    messageObject['urlLeaning'] = nameToLeanings[urlKey];
-  },
-  {
-    url: urlObject(nameToLeanings), //urlObject(nameToLeanings),
-  }
-);
+// // const port = chrome.runtime.connect({ name: 'defaultPort' });
+// const messageObject = { action: '', urlKey: '', urlLeaning: '' };
+// chrome.webNavigation.onCompleted.addListener(
+//   (details) => {
+//     let urlKey;
+//     for (i of Object.keys(nameToLeanings)) {
+//       if (details.url.includes(i)) {
+//         urlKey = i;
+//         break;
+//       }
+//     }
+//     console.log('Step 1');
+//     // chrome.runtime.sendMessage(
+//     //   {
+//     //     action: 'urlDetails',
+//     //     urlKey,
+//     //     urlLeaning: nameToLeanings[urlKey],
+//     //   },
+//     //   (response) => {
+//     //     // console.log(response.farewell);
+//     //   }
+//     // );
+//     messageObject['action'] = 'urlDetails';
+//     messageObject['urlKey'] = urlKey;
+//     messageObject['urlLeaning'] = nameToLeanings[urlKey];
+//   },
+//   {
+//     url: urlObject(nameToLeanings), //urlObject(nameToLeanings),
+//   }
+// );
 
-chrome.runtime.onConnect.addListener((port) => {
-  port.onMessage.addListener((msg) => {
-    if (msg.action == 'Initialise') {
-      port.postMessage(messageObject);
-    }
-  });
-});
+// chrome.runtime.onConnect.addListener((port) => {
+//   port.onMessage.addListener((msg) => {
+//     if (msg.action == 'Initialise') {
+//       port.postMessage(messageObject);
+//     }
+//   });
+// });
--- popup.js
+++ popup.js
@@ -5,64 +5,37 @@ const setLeaningsTag = (content) => {
   console.log('Inner HTML set');
 };
 
-/* chrome.webNavigation.onCompleted.addListener(
-  (details) => {
-    let urlKey;
-    for (i of Object.keys(nameToLeanings)) {
-      if (details.url.includes(i)) {
-        urlKey = i;
-        break;
-      }
-    }
-    console.log('Step 1');
-    setLeaningsTag(nameToLeanings[urlKey]);
-    // chrome.runtime.sendMessage(
-    //   {
-    //     action: 'urlDetails',
-    //     urlKey,
-    //     urlLeaning: nameToLeanings[urlKey],
-    //   },
-    //   (response) => {
-    //     // console.log(response.farewell);
-    //   }
-    // );
+const nameToLeanings = {
+  'bbc.com': 'left',
+  'cnn.com': 'left',
+  'foxnews.com': 'right',
+};
+const leaningsToName = { left: ['bbc.com', 'cnn.com'], right: ['foxnews.com'] };
 
-    // port.postMessage({
-    //   action: 'urlDetails',
-    //   urlKey,
-    //   urlLeaning: nameToLeanings[urlKey],
-    // });
-  },
-  {
-    url: urlObject(nameToLeanings), //urlObject(nameToLeanings),
+const urlObject = (dataObject) => {
+  const urlArray = [];
+  for (i of Object.keys(dataObject)) {
+    urlArray.push({ hostSuffix: i });
   }
-); */
 
-const port = chrome.runtime.connect({ name: 'defaultPort' });
-port.postMessage({ action: 'Initialise' });
-chrome.runtime.onConnect.addListener((port) => {
-  // chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log('Step 2');
-  port.onMessage.addListener((msg) => {
-    if (msg.action === 'urlDetails') {
-      console.log('Step 3');
-      setLeaningsTag(request.urlLeaning);
+  return urlArray;
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('loaded');
+  chrome.webNavigation.onCompleted.addListener(
+    (details) => {
+      let urlKey;
+      for (i of Object.keys(nameToLeanings)) {
+        if (details.url.includes(i)) {
+          urlKey = i;
+          break;
+        }
+      }
+      setLeaningsTag(urlKey);
+    },
+    {
+      url: urlObject(nameToLeanings), //urlObject(nameToLeanings),
     }
-    // if (request.action === 'urlDetails') {
-    //   console.log('Step 3');
-    //   setLeaningsTag(request.urlLeaning);
-    //   // sendResponse({ farewell: 'goodbye' });
-    //   // return true;
-    //   // this is required to use sendResponse asynchronously
-    //   // return true;
-
-    //   // if (port.name === 'defaultPort') {
-    //   //   port.onMessage.addListener((msg) => {
-    //   //     if (msg.action === 'urlDetails') {
-    //   //       setLeaningsTag(msg.urlLeaning);
-    //   //     }
-    //   //   });
-    //   // }
-    // }
-  });
+  );
 });
