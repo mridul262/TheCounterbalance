9876da37c3465087b5de197118cb140c51606188
Mridul Mahesh <mridulmahesh262@gmail.com>
2020-06-20 17:37:29 +0800

    Minor changes

--- popup.js
+++ popup.js
@@ -1,22 +1,11 @@
-const nameToLeanings = {
-  'bbc.com': 'left',
-  'cnn.com': 'left',
-  'foxnews.com': 'right',
-};
-const leaningsToName = { left: ['bbc.com', 'cnn.com'], right: ['foxnews.com'] };
-
-const urlObject = (dataObject) => {
-  const urlArray = [];
-  for (i of Object.keys(dataObject)) {
-    urlArray.push({ hostSuffix: i });
-  }
-
-  return urlArray;
+const setLeaningsTag = (content) => {
+  console.log('Step 4');
+  const leaningsTag = document.getElementById('leaning');
+  leaningsTag.innerHTML = content;
+  console.log('Inner HTML set');
 };
 
-// const port = chrome.runtime.connect({ name: 'defaultPort' });
-
-chrome.webNavigation.onCompleted.addListener(
+/* chrome.webNavigation.onCompleted.addListener(
   (details) => {
     let urlKey;
     for (i of Object.keys(nameToLeanings)) {
@@ -47,32 +36,33 @@ chrome.webNavigation.onCompleted.addListener(
   {
     url: urlObject(nameToLeanings), //urlObject(nameToLeanings),
   }
-);
+); */
 
-const setLeaningsTag = (content) => {
-  console.log('Step 4');
-  const leaningsTag = document.getElementById('leaning');
-  leaningsTag.innerHTML = content;
-  console.log('Inner HTML set');
-};
+const port = chrome.runtime.connect({ name: 'defaultPort' });
+port.postMessage({ action: 'Initialise' });
+chrome.runtime.onConnect.addListener((port) => {
+  // chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log('Step 2');
+  port.onMessage.addListener((msg) => {
+    if (msg.action === 'urlDetails') {
+      console.log('Step 3');
+      setLeaningsTag(request.urlLeaning);
+    }
+    // if (request.action === 'urlDetails') {
+    //   console.log('Step 3');
+    //   setLeaningsTag(request.urlLeaning);
+    //   // sendResponse({ farewell: 'goodbye' });
+    //   // return true;
+    //   // this is required to use sendResponse asynchronously
+    //   // return true;
 
-// // chrome.runtime.onConnect.addListener((port) => {
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   console.log('Step 2');
-//   if (request.action === 'urlDetails') {
-//     console.log('Step 3');
-//     setLeaningsTag(request.urlLeaning);
-//     // sendResponse({ farewell: 'goodbye' });
-//     return true;
-//     // this is required to use sendResponse asynchronously
-//     // return true;
-
-//     // if (port.name === 'defaultPort') {
-//     //   port.onMessage.addListener((msg) => {
-//     //     if (msg.action === 'urlDetails') {
-//     //       setLeaningsTag(msg.urlLeaning);
-//     //     }
-//     //   });
-//     // }
-//   }
-// });
+    //   // if (port.name === 'defaultPort') {
+    //   //   port.onMessage.addListener((msg) => {
+    //   //     if (msg.action === 'urlDetails') {
+    //   //       setLeaningsTag(msg.urlLeaning);
+    //   //     }
+    //   //   });
+    //   // }
+    // }
+  });
+});
