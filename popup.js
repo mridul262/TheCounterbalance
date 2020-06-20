81b1c937375940dba23f5e5e3d4a9f1935e2f5be
Mridul Mahesh <mridulmahesh262@gmail.com>
2020-06-20 18:30:01 +0800

    Working data exchange between background and popup.js

--- popup.js
+++ popup.js
@@ -5,37 +5,19 @@ const setLeaningsTag = (content) => {
   console.log('Inner HTML set');
 };
 
-const nameToLeanings = {
-  'bbc.com': 'left',
-  'cnn.com': 'left',
-  'foxnews.com': 'right',
-};
-const leaningsToName = { left: ['bbc.com', 'cnn.com'], right: ['foxnews.com'] };
-
-const urlObject = (dataObject) => {
-  const urlArray = [];
-  for (i of Object.keys(dataObject)) {
-    urlArray.push({ hostSuffix: i });
-  }
-
-  return urlArray;
-};
-
-window.addEventListener('DOMContentLoaded', () => {
-  console.log('loaded');
-  chrome.webNavigation.onCompleted.addListener(
-    (details) => {
-      let urlKey;
-      for (i of Object.keys(nameToLeanings)) {
-        if (details.url.includes(i)) {
-          urlKey = i;
-          break;
-        }
-      }
-      setLeaningsTag(urlKey);
-    },
-    {
-      url: urlObject(nameToLeanings), //urlObject(nameToLeanings),
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  const activeTab = tabs[0];
+  chrome.runtime.sendMessage(
+    { message: 'initialise', activeTab },
+    (response) => {
+      console.log(response.urlLeaning);
     }
   );
 });
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.message === 'urlData') {
+//     // console.log(request.urlLeaning);
+//     console.log('hellolololo');
+//   }
+// });
