da348b8fea2b703755427ac9942db676ee75a50f
rizhaow <rizhaow@gmail.com>
2020-06-21 11:38:12 +0800

    add error states

--- popup.js
+++ popup.js
@@ -17,8 +17,7 @@ const makeCard = (data) => {
   card.className = 'card';
   card.href = data.url;
   card.target = '_blank';
-  card.rel = 'noopener noreferrer'; 
-  
+  card.rel = 'noopener noreferrer';
 
   const makeCardImage = () => {
     const cardImage = document.createElement('div');
@@ -26,42 +25,41 @@ const makeCard = (data) => {
     cardImage.style.backgroundImage = `url('${data.urlToImage}')`;
     return cardImage;
   };
-  
+
   const makeCardContent = () => {
     const cardContent = document.createElement('div');
     cardContent.className = 'card__content';
-  
+
     const cardTitle = document.createElement('h3');
     cardTitle.className = 'card__title card__overflow-hidden';
     cardTitle.textContent = data.title;
-  
+
     const cardSite = document.createElement('p');
     cardSite.className = 'card__site card__overflow-hidden';
-    console.log(data);
 
-    cardSite.textContent = data.author;
-  
+    cardSite.textContent = data.source.name;
+
     const cardPreview = document.createElement('p');
     cardPreview.className = 'card__preview';
     cardPreview.textContent = data.content;
-  
+
     cardContent.appendChild(cardTitle);
     cardContent.appendChild(cardSite);
     cardContent.appendChild(cardPreview);
-  
+
     return cardContent;
-  }
+  };
 
   card.appendChild(makeCardImage());
   card.appendChild(makeCardContent());
 
   return card;
-}
+};
 
 const createCards = (data) => {
   const container = document.getElementById('container');
   for (const cardData of data.articles) {
-    container.appendChild(makeCard(cardData))
+    container.appendChild(makeCard(cardData));
   }
 };
 
@@ -70,7 +68,6 @@ const apiRequest = (articleRequest) => {
     method: 'GET',
     redirect: 'follow',
   };
-  console.log('reqQuery: ', articleRequest);
   let jsonResponse;
   fetch(newsAPI + articleRequest, requestOptions)
     .then((response) => response.json())
@@ -84,9 +81,24 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   chrome.runtime.sendMessage(
     { message: 'initialise', activeTab },
     (response) => {
-      console.log(response);
-      response.urlLeaning && setTag('title', response.urlLeaning);
-      apiRequest(getArticleForRequest(response.articleName));
+      if (response.urlLeaning) {
+        setTag('title', response.urlLeaning);
+        document.getElementById('title').textContent = response.urlLeaning;
+
+        if (response.articleName) {
+          document.getElementById('description').textContent =
+            'Take a look at the other side.';
+          apiRequest(getArticleForRequest(response.articleName));
+        } else {
+          document.getElementById('description').textContent =
+            "Can't find an alternate news sites. Try searching on google.";
+        }
+      } else {
+        document.getElementById('title').textContent = "Can't find a leaning";
+        document.getElementById('description').textContent =
+          'Come back next time when we have finished training our machine-learning algorithms.';
+        document.body.style.paddingBottom = '10px';
+      }
     }
   );
 });
